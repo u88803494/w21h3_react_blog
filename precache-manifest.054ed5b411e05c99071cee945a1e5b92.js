9ca16430bad9df0a3155f903cdd9f8f5fdfef2d4
Hugh <u88803494@gmail.com>
2019-11-24 21:43:55 +0800

    Updates

---
+++ precache-manifest.054ed5b411e05c99071cee945a1e5b92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b64f64436df9becfa3eb796961e396c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ca21fcdb8f88e9838856",
+    "url": "./static/css/main.9066b1f0.chunk.css"
+  },
+  {
+    "revision": "0e3d8d8e21eab64ff1d6",
+    "url": "./static/js/2.8b360e68.chunk.js"
+  },
+  {
+    "revision": "ca21fcdb8f88e9838856",
+    "url": "./static/js/main.660d259d.chunk.js"
+  },
+  {
+    "revision": "f56f25408540be801f77",
+    "url": "./static/js/runtime-main.0fb3dac0.js"
+  }
+]);
